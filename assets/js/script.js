c893a48f41e8805fec757b2d96f409ec4c6a4170
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-01-16 02:51:55 -0800

    finished layout

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+
+
+
+var searchInput = function() {
+	return document.querySelector('input').value;
+}
+
+
+
+
+// //1. create event listener to listen for user click using jQuery
+// var userFormEl = document.querySelector('#form-submit');
+// var nameInputEl = document.querySelector('#city-name');
+// var repoContainerEl = document.querySelector('#cities-container');
+// //var repoSearchTerm = document.querySelector('#repo-search-term');
+// var city = nameInputEl.innerhtml.value.trim();
+// console.log(city);
+
+// $(document).ready(function (event) {
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
+
+// //get value from input element
+
+
+
+// document.querySelector('button').addEventListener('click', function () {
+// fetchData();
+// })
+
+// function fetchData(event) {
+//   var apiUrl =
+//     'api.openweathermap.org/data/2.5/forecast?q=' +
+//     city +
+//     '&units=imperial' +
+//     '&appid=dfd70fe025fe63321e2acd91e52a5ebf';
+//   event.preventDefault();
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+// 			//convert fetch promise into JSON
+// 			response.json()
+// 			//send JSON data into callback function
+// 			.then(function (data) {
+//         displayWeather(data);
+//       });
+//     } else {
+//       alert('City not found. Please try again.');
+//     }
+//   });
+// }						
+
+// //var displayWeather = function (city, searchTerm)
